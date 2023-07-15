014cbe50cc15f52941ac314461cace435de607e5
uk-ar <yuuki.ari@gmail.com>
2023-07-15 20:14:03 +0900

    Enable event loop by select

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
 import socket
 from concurrent.futures import ThreadPoolExecutor
 import signal
+import select
 
 import app.reply
 
 def handle(c):
-    while True:
-        payload = c.recv(1024)
-        if not payload:
-            break
-        #print(payload)
-        resp = app.reply.reply(payload)
-        #print(resp)
-        c.send(resp)
-    print("Client disconnected")
+    #while True:
+    payload = c.recv(1024)
+    if not payload:
+        print("Client disconnected")
+        return
+    #print(payload)
+    resp = app.reply.reply(payload)
+    #print(resp)
+    c.send(resp)
+
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
@@ -22,19 +24,24 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-
-    
-
-
 class RedisServer:
+
     def __init__(self):
-        self.server_socket = socket.create_server(("localhost", 6379),
-                                             reuse_port=True)
+        self.server = socket.create_server(("localhost", 6379),
+                                           reuse_port=True)
+        self.client = []
+
     def listen(self):
-        tpe = ThreadPoolExecutor(max_workers=3)
         while True:
-            c, _ = self.server_socket.accept()  # wait for client
-            tpe.submit(handle, c)
+            fds_to_watch = [self.server, *self.client]
+            ready_to_read, _, _ = select.select(fds_to_watch, [], [])
+            for ready in ready_to_read:
+                if ready == self.server:
+                    con, addr = self.server.accept()
+                    print("New connection from", addr)
+                    self.client.append(con)
+                else:
+                    handle(ready)
 
 if __name__ == "__main__":
     s = RedisServer()
